96ea3899f2a52724bbed43f525974c6d6f65fdf5
galapagous <mmusamuhammed450@gmail.com>
2026-01-18 01:59:37 +0100

    chore: missing data update

--- src/application/use-case/course-case/GetCourseUseCase.ts
+++ src/application/use-case/course-case/GetCourseUseCase.ts
@@ -1,12 +1,28 @@
 import { CourseResponseDTO } from "@/application/dto/CourseDTOs";
 import { Course } from "@/domain/entities/course";
 import { CourseRepository } from "@/domain/repositories/CourseRepository";
+import { DocRepository } from "@/domain/repositories/DocRepository";
 
 export class GetCourseUserCase {
-  constructor(private courseRepository: CourseRepository) {}
+  constructor(
+    private courseRepository: CourseRepository,
+    private DocRepository: DocRepository
+  ) {}
 
   async execute(dto: string): Promise<CourseResponseDTO[]> {
-    const courses = this.courseRepository.findByOwner(dto);
-    return courses as unknown as Course[];
+    const courses = await this.courseRepository.findByOwner(dto);
+    let doc = null;
+    if (courses !== null) {
+      doc = await Promise.all(
+        courses?.map(async (course: any) => {
+          return await this.DocRepository.findByCourseId(course.id);
+        })
+      );
+    }
+    const result = courses?.map((course, index) => ({
+      ...course,
+      doc: doc ? doc[index]?.length : 0,
+    }));
+    return { result } as unknown as Course[];
   }
 }
